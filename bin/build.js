08d6dac59c704a0418a368f4844dc793801d4e72
JiM-W <550116990@qq.com>
2019-12-02 15:25:40 +0800

    修改发布脚本

--- bin/build.js
+++ bin/build.js
@@ -3,10 +3,11 @@ const path = require('path');
 let jsonPath = path.resolve(__dirname,'../package/package.json');
 let result = JSON.parse(fs.readFileSync(jsonPath, {encoding:'utf8'}));
 let npmType = process.argv[2];
+
 if(npmType === 'npm'){
-  result.name = "light-ui"
+  result.name = "light-cml-ui"
 }else{
-  result.name = "@didi/light-ui"
+  result.name = "@didi/light-cml-ui"
 }
 console.log(process.argv)
 fs.writeFileSync(jsonPath, JSON.stringify(result,null,2),{encoding:'utf8'})
--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo $1
 rm -rf ./package/components
 rm -rf ./package/assets
 cp -R ./src/components/ ./package/components
 cp -R  ./src/assets/ ./package/assets
+#如果是发布dnpm 内网源，那么将 import cml from 'chameleon-api' 替换为 import cml from '@didi/chameleon-api'
+if [ $1 -eq 'dnpm' ];then
+sed -i '' 's/chameleon-api/@didi\/chameleon-api./g'  ./package/**/**/*.js
+sed -i '' 's/chameleon-api/@didi\/chameleon-api./g'  ./package/**/**/*.cml
