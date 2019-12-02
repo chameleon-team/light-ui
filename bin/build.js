fb88a92794cba833b8b79396a165d92ec46c48df
JiM-W <550116990@qq.com>
2019-12-02 16:07:54 +0800

    subpage

--- bin/build.js
+++ bin/build.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 let jsonPath = path.resolve(__dirname,'../package/package.json');
 let result = JSON.parse(fs.readFileSync(jsonPath, {encoding:'utf8'}));
-let npmType = process.argv[2];
-
-if(npmType === 'npm'){
+const argv = require('minimist')(process.argv);
+console.log('argv',argv)
+if(argv.npm){ //发布npm源的包，修改包名
   result.name = "light-cml-ui"
 }else{
   result.name = "@didi/light-cml-ui"
